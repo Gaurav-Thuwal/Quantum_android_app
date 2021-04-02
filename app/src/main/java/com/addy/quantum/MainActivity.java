4e4a8ef4b3e658c01bff93127ae1744baf7b95cc
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-02 22:57:17 +0530

    app: added input fields
    Added GUI input field, and an intent to open AddActivity activity.

--- app/src/main/java/com/addy/quantum/MainActivity.java
+++ app/src/main/java/com/addy/quantum/MainActivity.java
@@ -2,13 +2,32 @@ package com.addy.quantum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize variables
+        add_button = findViewById(R.id.add_button);
+
+        // Click listener for floating button
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

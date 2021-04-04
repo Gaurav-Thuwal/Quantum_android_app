b148c096075281c853b44b828f3b1d53503e5d0e
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-04 14:56:11 +0530

    app: implement getData method for getting current date
    * in String format
    * init some view vars.

--- app/src/main/java/com/addy/quantum/AddActivity.java
+++ app/src/main/java/com/addy/quantum/AddActivity.java
@@ -3,12 +3,33 @@ package com.addy.quantum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AddActivity extends AppCompatActivity {
+
+    private EditText expense_name_input, expense_amount_input ;
+    private Button add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        //init vars
+        expense_name_input = findViewById(R.id.expense_name_input);
+        expense_amount_input = findViewById(R.id.expense_amount_input);
+        add_button = findViewById(R.id.add_button);
+
+    }
+
+    public String getDate(){
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        return simpleDateFormat.format(date);
     }
 }

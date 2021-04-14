d364c931eb06f2095d42156db294128e2f2c95a3
adnan-44 <mohd.adnan0405@gmail.com>
2021-04-14 23:41:56 +0530

    app: activity: update: Added input fields and get refrence to their objects in update_activity
    Init vars as of now

--- app/src/main/java/com/addy/quantum/UpdateActivity.java
+++ app/src/main/java/com/addy/quantum/UpdateActivity.java
@@ -3,12 +3,19 @@ package com.addy.quantum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class UpdateActivity extends AppCompatActivity {
+
+    private EditText expense_name_input, expense_amount_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        // init vars
+        expense_name_input = findViewById(R.id.expense_name_input);
+        expense_amount_input = findViewById(R.id.expense_amount_input);
     }
 }

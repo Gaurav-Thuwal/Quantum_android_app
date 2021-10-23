abf0ac7d7ba307557af21c224096b96eae29bd74
adnan-44 <mohd.adnan0405@gmail.com>
2021-10-23 10:52:00 +0530

    app: Added blank ThisMonthActivity
    * This Activity will be used to show expenses of current month, etc.
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

---
+++ app/src/main/java/com/addy/quantum/ThisMonthActivity.java
@@ -0,0 +1,14 @@
+package com.addy.quantum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThisMonthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_this_month);
+    }
+}

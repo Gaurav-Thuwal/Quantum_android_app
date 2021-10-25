59623b21290af0817f21e3d28a86bc5ba82c05c1
adnan-44 <mohd.adnan0405@gmail.com>
2021-10-26 01:26:00 +0530

    app: Show "Logout" button when account is signed in
    instead of login screen items (invisible the login items) and show "logout" button there
    * LoginActivity: add checks for whether an account is already signed in or not, if signed then show logout items else show login items in "LoginActivity"
    * actvity_login : Added one logout button and textView, both are invisible by default
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

--- app/src/main/java/com/addy/quantum/LoginActivity.java
+++ app/src/main/java/com/addy/quantum/LoginActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,8 +20,9 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity {
 
     // UI elements
-    private Button login_button, signup_button;
+    private Button login_button, signup_button, logout_button;
     private EditText email_input, password_input;
+    private TextView loginAccountText, orText, logged_in_text;
     private Toolbar toolbar;
 
     // Firebase authentication stuff
@@ -37,11 +39,27 @@ public class LoginActivity extends AppCompatActivity {
         login_button = findViewById(R.id.login_button);
         signup_button = findViewById(R.id.signup_button);
         toolbar = findViewById(R.id.toolbar);
+        logout_button = findViewById(R.id.logout_button);
+        loginAccountText = findViewById(R.id.textView);
+        orText = findViewById(R.id.textView2);
+        logged_in_text = findViewById(R.id.logged_in_text);
         firebaseAuth = FirebaseAuth.getInstance();
 
         // Set our custom toolbar as action bar
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // ALso check whether a account is already signed in or not, if it is then make all login items invisible
+        if(firebaseAuth.getCurrentUser() != null){
+            loginAccountText.setVisibility(View.INVISIBLE);
+            orText.setVisibility(View.INVISIBLE);
+            login_button.setVisibility(View.INVISIBLE);
+            signup_button.setVisibility(View.INVISIBLE);
+            email_input.setVisibility(View.INVISIBLE);
+            password_input.setVisibility(View.INVISIBLE);
+            logged_in_text.setVisibility(View.VISIBLE);
+            logout_button.setVisibility(View.VISIBLE);
+        }
 
         // login button functionality
         login_button.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +73,15 @@ public class LoginActivity extends AppCompatActivity {
                         // If task is done means account is logged in successfully, so show toasts accordingly
                         if(task.isSuccessful()){
                             Toast.makeText(LoginActivity.this, "Account logged in successfully", Toast.LENGTH_SHORT).show();
+                            // Hide all the login items once account is logged-in, make logout button and textview visible
+                            loginAccountText.setVisibility(View.INVISIBLE);
+                            orText.setVisibility(View.INVISIBLE);
+                            login_button.setVisibility(View.INVISIBLE);
+                            signup_button.setVisibility(View.INVISIBLE);
+                            email_input.setVisibility(View.INVISIBLE);
+                            password_input.setVisibility(View.INVISIBLE);
+                            logged_in_text.setVisibility(View.VISIBLE);
+                            logout_button.setVisibility(View.VISIBLE);
                             finish();
                         } else {
                             Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
@@ -71,6 +98,26 @@ public class LoginActivity extends AppCompatActivity {
                 // Open SignUpActivity to create new user
                 Intent signUp = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(signUp);
+            }
+        });
+
+        // logout button functionality
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log out the current user on tap of logout button
+                firebaseAuth.signOut();
+                Toast.makeText(LoginActivity.this, "Account logged out", Toast.LENGTH_LONG).show();
+
+                // And make all login items visible and logout items invisible
+                loginAccountText.setVisibility(View.VISIBLE);
+                orText.setVisibility(View.VISIBLE);
+                login_button.setVisibility(View.VISIBLE);
+                signup_button.setVisibility(View.VISIBLE);
+                email_input.setVisibility(View.VISIBLE);
+                password_input.setVisibility(View.VISIBLE);
+                logged_in_text.setVisibility(View.INVISIBLE);
+                logout_button.setVisibility(View.INVISIBLE);
             }
         });
     }

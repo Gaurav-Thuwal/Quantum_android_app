c52d0ee2483f5313ce72b572692f5e3976b6bd13
adnan-44 <mohd.adnan0405@gmail.com>
2021-10-26 22:50:36 +0530

    app: UploadActivity: Show formatted datetime for last updated cloud database
    * Added TextViews to show formatted date time.
    * Implemented separate method to get and set last_updated value in textViews.
    * Show live update of datetime of last_updated, also Don't close the UploadActivity after savind data to cloud.
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

--- app/src/main/java/com/addy/quantum/UploadActivity.java
+++ app/src/main/java/com/addy/quantum/UploadActivity.java
@@ -8,13 +8,22 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +32,7 @@ public class UploadActivity extends AppCompatActivity {
     // UI stuff
     private Button upload;
     private Toolbar toolbar;
+    private TextView lastUpdated;
 
     // Firebase stuff
     private FirebaseAuth firebaseAuth;
@@ -37,6 +47,7 @@ public class UploadActivity extends AppCompatActivity {
         // Init vars
         upload = findViewById(R.id.upload_button);
         toolbar = findViewById(R.id.toolbar);
+        lastUpdated = findViewById(R.id.last_updated);
         firebaseAuth = FirebaseAuth.getInstance();
 
         // Get firebase database reference as "expenses/user_uid", because we'll save user expenses there
@@ -45,6 +56,9 @@ public class UploadActivity extends AppCompatActivity {
         // Set our custom toolbar as ActionBar
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Check for latest info about last updated from database
+        getLastUpdated();
 
         // upload button functionality
         upload.setOnClickListener(new View.OnClickListener() {
@@ -73,7 +87,12 @@ public class UploadActivity extends AppCompatActivity {
             data.put(DatabaseHelper.COLUMN_DATE, cursor.getString(3));      // expense_date column
 
             // Create node name as _id value and save the data (as child)
-            databaseReference.child(cursor.getString(0)).setValue(data).addOnFailureListener(new OnFailureListener() {
+            databaseReference.child(cursor.getString(0)).setValue(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    setLatestUpdated();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
                     // This method gets trigger when data is failed to save in firebase database, show toast accordingly
@@ -88,9 +107,43 @@ public class UploadActivity extends AppCompatActivity {
         // Show toast according to upload status
         if(uploadSuccessful){
             Toast.makeText(UploadActivity.this, "Successfully saved data to cloud", Toast.LENGTH_LONG).show();
-            finish();
         } else {
             Toast.makeText(UploadActivity.this, "Failed to save the data to cloud", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    // Method to get last updated status of firebase database and update last_updated textView
+    private void getLastUpdated(){
+        FirebaseDatabase.getInstance().getReference("users/"+firebaseAuth.getCurrentUser().getUid())
+                .child("last_updated").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String data = snapshot.getValue(String.class);
+                if(data != null){
+                    // get the datetime from snapshot and set into the lastUpdated TextView
+                    lastUpdated.setText(getFormattedTime(data));
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+    }
+
+    // Method to set last_updated datetime in database
+    private void setLatestUpdated(){
+        String currentTime = String.valueOf(System.currentTimeMillis());
+        FirebaseDatabase.getInstance().getReference("users/"+firebaseAuth.getCurrentUser().getUid())
+                .child("last_updated").setValue(currentTime).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(!task.isSuccessful()) Toast.makeText(UploadActivity.this, "Failed to update latest time", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Method to return formatted datetime string
+    private String getFormattedTime(String dateTime){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy hh:mm:ss a");
+        return String.valueOf(dateFormat.format(new Date(Long.parseLong(dateTime))));
     }
 }
